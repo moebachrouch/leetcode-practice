acab98c3ca392afbae47f12edf64e0bdf52bd122
moebachrouch <moe.bachrouch@outlook.com>
2021-08-03 02:00:02 -0400

    problem13

---
+++ problem13/Solution.java
@@ -0,0 +1,42 @@
+import java.util.Map;
+import java.util.HashMap;
+
+public class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.romanToInt("MCMXCIV"));
+    }
+
+    public int romanToInt(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+
+        int res = 0;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            char c = s.charAt(i);
+            int integer = map.get(c);
+
+            if (i > 0) {
+                char previousChar = s.charAt(i - 1);
+                int previousInteger = map.get(previousChar);
+                if (integer > previousInteger) {
+                    res += integer - previousInteger;
+                    i--;
+                } else {
+                    res += integer;
+                }
+            } else {
+                res += integer;
+            }
+        }
+
+        return res;
+    }
+}
