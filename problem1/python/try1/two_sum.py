d292c7259656f018d98f5b81c0d06bbdfead03f7
moebachrouch <moe.bachrouch@outlook.com>
2021-10-10 18:25:15 -0400

    working solution for problem1

---
+++ problem1/python/try1/two_sum.py
@@ -0,0 +1,10 @@
+# time limit exceeded
+
+def twoSum(nums, target):
+    for i in range(len(nums)):
+        for j in range(len(nums)):
+            if i != j and nums[i] + nums[j] == target:
+                return [i, j]
+
+
+print(twoSum([3, 3], 6))
---
+++ problem1/python/try2/two_sum.py
@@ -0,0 +1,11 @@
+def twoSum(nums, target):
+    map = {}
+    for i in range(len(nums)):
+        complement = target - nums[i]
+        if nums[i] not in map:
+            map[nums[i]] = i
+        if complement in map and map[complement] != i and nums[map[complement]] + nums[i] == target:
+            return [map[complement], i]
+
+
+print(twoSum([2, 7, 11, 15], 9))
