4c24c3445d9bb42b9cc320e0a224749997fce17f
moebachrouch <moe.bachrouch@outlook.com>
2021-10-11 19:54:43 -0400

    solution with map

---
+++ problem217/try2/contains_duplicate.py
@@ -0,0 +1,12 @@
+def contains_duplicate(nums):
+    contains = {}
+    for num in nums:
+        if num in contains:
+            return True
+        else:
+            contains[num] = False
+    return False
+
+
+print(contains_duplicate([1, 2, 3, 4]))
+print(contains_duplicate([1, 2, 3, 1]))
