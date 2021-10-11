dd1a5614b495d7c73d2a271e27795409d4fdcf37
moebachrouch <moe.bachrouch@outlook.com>
2021-10-11 19:51:56 -0400

    contains duplicate solution

---
+++ problem217/try1/contains_duplicate.py
@@ -0,0 +1,10 @@
+def contains_duplicate(nums):
+    contains = set()
+    for num in nums:
+        if num in contains:
+            return True
+        else:
+            contains.add(num)
+    return False
+
+print(contains_duplicate([1,2,3,4]))
