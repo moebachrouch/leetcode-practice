50bdbd8c9cf7b34a484943430fca79ad08e5cde1
moebachrouch <moe.bachrouch@outlook.com>
2021-10-11 20:18:11 -0400

    solution 4

--- problem217/try3/contains_duplicate.py
+++ problem217/try3/contains_duplicate.py
@@ -1,11 +1,12 @@
 def contains_duplicate(nums):
-    contains = set()
+    contains = {}
     for num in nums:
         if num in contains:
             return True
         else:
-            contains.add(num)
+            contains[num] = False
     return False
 
 
 print(contains_duplicate([1, 2, 3, 4]))
+print(contains_duplicate([1, 2, 3, 1]))
---
+++ problem217/try4/contains_duplicate.py
@@ -0,0 +1,10 @@
+def contains_duplicate(nums):
+    lst = sorted(nums)
+    for i in range(len(nums) - 1):
+        if lst[i] == lst[i + 1]:
+            return True
+    return False
+
+
+print(contains_duplicate([1, 2, 3, 4]))
+print(contains_duplicate([1, 2, 3, 1]))
